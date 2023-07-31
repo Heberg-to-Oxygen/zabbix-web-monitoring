e2c063f27857b2135700cab977a226e6547f84ee
Florian-DJ <floriandjerbi@gmail.com>
2023-07-31 19:40:54 +0200

    Add file sql structure

--- zabbix_web/README.md
+++ zabbix_web/README.md
@@ -3,7 +3,7 @@
 - Author : DJERBI Florian
 - Object : Monitoring a website on Zabbix 
 - Creation Date : 07/27/2023
-- Modification Date : 07/27/2023
+- Modification Date : 07/31/2023
 
 
 ## Install
@@ -27,5 +27,16 @@ useradd -m -s /bin/bash -d /home/zabbix_web zabbix_web
 su - zaabix_web
 rm -rf .*
 git clone --branch zabbix_web --single-branch https://github.com/Florian-Dj/script-infra.git
+```
+### Database
+Create a username with the privilieges
+``` 
+
+```
+
+Insert a sql structure file in database
+
+``` bash
+sudo mysql -u `username` -p zabbix_web < zabbix_web.struct.sql
 ```
 
---
+++ zabbix_web/zabbix_web.struct.sql
@@ -0,0 +1,36 @@
+/* Create a zabbix_web table */
+CREATE DATABASE `zabbix_web`;
+
+USE `zabbix_web`;
+
+
+/* Create a server table */
+DROP TABLE IF EXISTS `server`;
+
+CREATE TABLE `server` (
+  `server_id` varchar(64) NOT NULL,
+  `server_name` varchar(255) NOT NULL,
+  `server_support` varchar(40) DEFAULT NULL,
+  `server_screen` smallint(6) DEFAULT NULL,
+  UNIQUE KEY `server_name` (`server_name`),
+  UNIQUE KEY `server_screen` (`server_screen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+/* Create a web table */
+DROP TABLE IF EXISTS `web`;
+
+CREATE TABLE `web` (
+  `web_id` int(11) NOT NULL AUTO_INCREMENT,
+  `web_domain` varchar(255) NOT NULL,
+  `web_url` varchar(255) NOT NULL,
+  `php_version` varchar(16) DEFAULT NULL,
+  `web_env` varchar(16) NOT NULL,
+  `web_support` varchar(40) NOT NULL,
+  `web_status` varchar(40) DEFAULT 'enabled',
+  `web_server` varchar(64) NOT NULL,
+  `web_created` datetime NOT NULL,
+  `web_update` datetime DEFAULT NULL,
+  PRIMARY KEY (`web_id`),
+  UNIQUE KEY `web_domain` (`web_domain`)
+) ENGINE=InnoDB AUTO_INCREMENT=228 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
