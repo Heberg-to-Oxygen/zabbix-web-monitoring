66373092e74a2329229c2df74f8d8afc72b52af6
Florian-DJ <floriandjerbi@gmail.com>
2023-08-10 19:31:30 +0200

    Column name patch web_update to web_updated

--- zabbix_web/zabbix_web.struct.sql
+++ zabbix_web/zabbix_web.struct.sql
@@ -3,7 +3,7 @@
 # Author : DJERBI Florian
 # Object : Management a website with zabbix discovery
 # Creation Date : 07/29/2023
-# Modification Date : 08/08/2023
+# Modification Date : 08/10/2023
 ###########################
 */
 
@@ -13,9 +13,10 @@ CREATE DATABASE IF NOT EXISTS `zabbix_web`;
 USE `zabbix_web`;
 
 
-/* Create a server table */
+/* Drop the server table */
 DROP TABLE IF EXISTS `server`;
 
+/* Create a server table */
 CREATE TABLE `server` (
   `server_id` varchar(64) NOT NULL,
   `server_name` varchar(255) NOT NULL,
@@ -25,9 +26,10 @@ CREATE TABLE `server` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-/* Create a web table */
+/* Drop the website table */
 DROP TABLE IF EXISTS `web`;
 
+/* Create a web table */
 CREATE TABLE `web` (
   `web_id` int(11) NOT NULL AUTO_INCREMENT,
   `web_domain` varchar(255) NOT NULL,
@@ -38,7 +40,7 @@ CREATE TABLE `web` (
   `web_status` BOOLEAN DEFAULT 1,
   `web_server` varchar(64) NOT NULL,
   `web_created` datetime NOT NULL,
-  `web_update` datetime DEFAULT NULL,
+  `web_updated` datetime DEFAULT NULL,
   PRIMARY KEY (`web_id`),
   UNIQUE KEY `web_domain` (`web_domain`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
